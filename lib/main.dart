7bb29704c18d1f8de873181dbb923e10a1e9666b
triste <tristecsv@gmail.com>
2025-08-13 23:58:27 -0600

    feat: drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:mylife/shared/widgets/drawer/app_drawer.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final drawerController = MyDrawerController();
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: ZoomDrawer(
+          controller: drawerController,
+          menuBackgroundColor: Colors.black38,
+          menuScreen: Container(
+            color: Colors.transparent,
+            child: Column(
+              children: [
+                const SizedBox(height: 80),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  onTap: () => drawerController.close!(),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () => drawerController.close!(),
+                ),
+              ],
+            ),
+          ),
+          mainScreen: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () => drawerController.toggle!(),
+              ),
+              title: const Text('myLife'),
+            ),
+            body: const Center(child: Text('Main content')),
+          ),
         ),
       ),
     );
